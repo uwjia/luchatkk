cf9a4321fb69407e2dc6e5935d7fde92d1014f02
uwjia <1197788400@qq.com>
2022-05-23 00:06:08 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/uwjia/luchatkk
+
+go 1.17
+
+require github.com/uwjia/lufirst v0.0.1
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/uwjia/lufirst/somefm"
+)
+
+func main() {
+	fmt.Println("this is luchatkk")
+	somefm.PrintHello()
+}
